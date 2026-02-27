82a51086392850b88b682005233eb5462ae1001c
Can Güvendiren <can@glacion.com>
2026-02-27 11:18:44 +0300

    feat: move to cargo2nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix/release-0.12";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
   };
 
   outputs =
@@ -11,6 +16,7 @@
       nixpkgs,
       flake-utils,
       rust-overlay,
+      cargo2nix,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -21,7 +27,10 @@
 
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
+          overlays = [
+            rust-overlay.overlays.default
+            cargo2nix.overlays.default
+          ];
         };
 
         rustToolchain = pkgs.rust-bin.selectLatestNightlyWith (
@@ -35,14 +44,20 @@
             ];
           }
         );
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = pkgs.rust-bin.stable.latest.default.version;
+          packageFun = import ./Cargo.nix;
+          packageOverrides = pkgs: [
+            pkgs.rustBuilder.overrides.capLints
+          ];
+        };
+
+        package = rustPkgs.workspace.reloader { };
       in
       {
         packages = rec {
-          default = pkgs.rustPlatform.buildRustPackage {
-            inherit name version;
-            cargoLock.lockFile = ./Cargo.lock;
-            src = ./.;
-          };
+          default = package;
           stream = pkgs.dockerTools.streamLayeredImage {
             inherit name;
             tag = version;
@@ -58,14 +73,29 @@
           };
         };
 
-        devShells = {
-          default = pkgs.mkShell {
-            packages = [
-              rustToolchain
-              pkgs.direnv
-              pkgs.skopeo
-            ];
-          };
+        checks.clippy = pkgs.rustPlatform.buildRustPackage {
+          inherit version;
+          pname = "clippy";
+          doCheck = true;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          nativeBuildInputs = [
+            rustToolchain
+          ];
+          checkPhase = ''
+            runHook preCheck
+            cargo clippy --workspace --all-targets --offline --locked -- -D warnings
+            runHook postCheck
+          '';
+        };
+
+        devShells.default = rustPkgs.workspaceShell {
+          nativeBuildInputs = [
+            rustToolchain
+            pkgs.cargo
+            pkgs.direnv
+            pkgs.skopeo
+          ];
         };
       }
     );
